a261e2aeb7237114c28987e5b918fcd5e6ad12b9
Niv Shani <nivshani08@gmail.com>
2020-09-27 10:29:57 +0300

    basic single task component

---
+++ client/src/components/Task.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="task-wrapper">
+        <span>{{ task.title }}</span>
+        <span>{{ task.isDone }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue, {PropType} from "vue";
+    import Component from "vue-class-component";
+    import {Prop} from "vue-property-decorator";
+
+    @Component
+    export default class Task extends Vue {
+        @Prop({ type: Object as PropType<Task> }) task!;
+    }
+</script>
+
+<style scoped lang="scss">
+    .task-wrapper {
+        padding: 1rem 2rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        transition: all .2s 0s ease-out;
+        &:hover {
+            cursor: pointer;
+            background-color: lighten($primary, 30%);
+            border-color: $primary;
+        }
+    }
+</style>
--- client/src/components/TasksContainer.vue
+++ client/src/components/TasksContainer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        {{ tasks }}
+        <Task v-for="(task, i) in tasks" :task="task" :key="i" class="task" />
     </div>
 </template>
 
@@ -9,13 +9,18 @@
     import Component from "vue-class-component";
     import {Prop} from "vue-property-decorator";
     import TasksCollection from "../../../server/src/models/TasksCollection";
-    import Task from "../../../server/src/models/Task";
+    import Task from "@/components/Task";
+    import TaskModel from "../../../server/src/models/Task";
 
-    @Component
+    @Component({
+        components: {
+            Task
+        }
+    })
     export default class TasksContainer extends Vue {
-        @Prop({ type: Object as PropType<TasksCollection> }) tasksCollection;
+        @Prop({ type: Object as PropType<TasksCollection> }) tasksCollection!;
 
-        get tasks(): Task[] {
+        get tasks(): TaskModel[] {
             return this.tasksCollection?.getAll();
         }
     }
@@ -28,5 +33,8 @@
         width: 60%;
         padding: 2rem 3rem;
         box-shadow: 0 0 30px transparentize($darkprimary, .4);
+    }
+    .task:not(:last-of-type) {
+        border-bottom: 1px solid darken($foreground, 5%);
     }
 </style>
